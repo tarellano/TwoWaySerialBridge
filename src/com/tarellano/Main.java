1e0d8cfc7ec4bf04078ac13378c0c9e78fc772fe
Thomas Arellano <Thomas.G.Arellano@gmail.com>
2016-06-06 19:57:04 -0400

    Initial commit

---
+++ src/com/tarellano/Main.java
@@ -0,0 +1,8 @@
+package com.tarellano;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/tarellano/SerialBridge.java
@@ -0,0 +1,11 @@
+package com.tarellano;
+
+/**
+ * Created by Thomas Arellano on 2016-06-06.
+ */
+public class SerialBridge {
+
+    public void bridgeSerialPorts(String portA, String portB){
+
+    }
+}
