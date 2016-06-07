eaad840f5e7b4e92eac5121e3bdc8531b663be1c
Thomas Arellano <Thomas.G.Arellano@gmail.com>
2016-06-06 20:57:19 -0400

    Added Simultaneous read/write

--- src/com/tarellano/Main.java
+++ src/com/tarellano/Main.java
@@ -1,8 +1,30 @@
 package com.tarellano;
 
+import gnu.io.NoSuchPortException;
+import gnu.io.PortInUseException;
+import gnu.io.SerialPort;
+import gnu.io.UnsupportedCommOperationException;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        SerialBridge serialBridge = new SerialBridge();
+        try {
+            SerialPort serialPortA = SerialBridge.getSerialPort("COM5", 38400, SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+            SerialPort serialPortB = SerialBridge.getSerialPort("COM6", 38400, SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+            serialBridge.bridgeSerialPorts(serialPortA, serialPortB);
+        } catch (NoSuchPortException e) {
+            e.printStackTrace();
+        } catch (PortInUseException e) {
+            e.printStackTrace();
+        } catch (UnsupportedCommOperationException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
--- src/com/tarellano/SerialBridge.java
+++ src/com/tarellano/SerialBridge.java
@@ -2,23 +2,65 @@ package com.tarellano;
 
 import gnu.io.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
  * Created by Thomas Arellano on 2016-06-06.
  */
 public class SerialBridge {
 
-    public void bridgeSerialPorts(SerialPort serialPortA, SerialPort serialPortB){
+    public void bridgeSerialPorts(SerialPort serialPortA, SerialPort serialPortB) throws IOException {
+
+        InputStream serialPortInA = serialPortA.getInputStream();
+        InputStream serialPortInB = serialPortB.getInputStream();
+        OutputStream serialPortOutA = serialPortA.getOutputStream();
+        OutputStream serialPortOutB = serialPortB.getOutputStream();
+
+        Thread serialThreadAtoB = new SerialPortReadWrite(serialPortInA, serialPortOutB);
+        Thread serialThreadBtoA = new SerialPortReadWrite(serialPortInB, serialPortOutA);
 
     }
 
-    public SerialPort getSerialPort(String portName, int baudRate, int dataBits, int stopBits, int parityBits)
+    public class SerialPortReadWrite extends Thread {
+
+        private InputStream in;
+        private OutputStream out;
+
+        public SerialPortReadWrite(InputStream in, OutputStream out)
+        {
+            this.in = in;
+            this.out = out;
+        }
+
+        @Override
+        public void run(){
+            byte[] buff = new byte[128];
+            int b;
+            try {
+                while ((b = in.read(buff)) > -1){
+                    out.write(buff, 0, b);
+                    System.out.print(new String(buff, 0, b));
+                }
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public static SerialPort getSerialPort(String portName, int baudRate, int dataBits, int stopBits, int parityBits)
             throws NoSuchPortException, PortInUseException, UnsupportedCommOperationException {
         CommPortIdentifier commPortIdentifier = CommPortIdentifier.getPortIdentifier(portName);
         if (commPortIdentifier.isCurrentlyOwned()){
             System.out.println(portName + " is already in use");
+            return null;
         }
 
-        CommPort commPort = commPortIdentifier.open(this.getClass().getName(), 2000);
+        CommPort commPort = commPortIdentifier.open(SerialBridge.class.getName() , 2000);
         SerialPort serialPort = (SerialPort) commPort;
 
         serialPort.setSerialPortParams(baudRate, dataBits, stopBits, parityBits);
